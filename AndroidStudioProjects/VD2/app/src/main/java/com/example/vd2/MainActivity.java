2f38c9b83a871b7b814bd6f903ceddb3bdb7d4d5
Zinxus <hieu123lio71@gmail.com>
2024-03-09 14:50:21 +0700

    AppTinhToanDonGian

--- AndroidStudioProjects/VD2/app/src/main/java/com/example/vd2/MainActivity.java
+++ AndroidStudioProjects/VD2/app/src/main/java/com/example/vd2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.vd2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,28 +12,86 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Khai bao cac đối tượng gần cới đkhiển tương ứng ở đây
+    EditText editTextsoA;
+    EditText editTextsoB;
+    EditText editTextKQ;
+    Button nutCong,nutTru,nutNhan,nutChia;
+    public void Timdieukhien() {
+        editTextsoA = (EditText) findViewById(R.id.edtso1);
+        editTextsoB = (EditText) findViewById(R.id.edtso2);
+        editTextKQ = findViewById(R.id.edtKQ);
+        nutCong = (Button) findViewById(R.id.btnCong);
+        nutTru = (Button) findViewById(R.id.btnTru);
+        nutNhan = (Button) findViewById(R.id.btnNhan);
+        nutChia = (Button) findViewById(R.id.btnChia);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Timdieukhien();
     }
-    void XulyCong(View view){
 
+    public void XulyCong(View view){
+
+        //Lay du lieu ve o dieu khien ve a
+        String strsoA = editTextsoA.getText().toString();
+        //Lay du lieu ve o dieu khien ve b
+        String strsoB =editTextsoB.getText().toString();
+        //Chuyen du lieu sang dang so
+        float so_a = Float.parseFloat(strsoA);
+        float so_b = Float.parseFloat(strsoB);
+        //Tinh toan theo yeu cau
+        float Tong  = so_a + so_b;
+        String strTong = String.valueOf(Tong);
+        //Hien thi ra man hinh
+        editTextKQ.setText(strTong);
     }
-    void XulyTru(View view){
+    public void XulyTru(View view){
 
+        //Lay du lieu ve o dieu khien ve a
+        String strsoA = editTextsoA.getText().toString();
+        //Lay du lieu ve o dieu khien ve b
+        String strsoB =editTextsoB.getText().toString();
+        //Chuyen du lieu sang dang so
+        float so_a = Float.parseFloat(strsoA);
+        float so_b = Float.parseFloat(strsoB);
+        //Tinh toan theo yeu cau
+        float Tong  = so_a - so_b;
+        String strTong = String.valueOf(Tong);
+        //Hien thi ra man hinh
+        editTextKQ.setText(strTong);
     }
-    void XulyNhan(View view){
+    public void XulyNhan(View view){
 
+        //Lay du lieu ve o dieu khien ve a
+        String strsoA = editTextsoA.getText().toString();
+        //Lay du lieu ve o dieu khien ve b
+        String strsoB =editTextsoB.getText().toString();
+        //Chuyen du lieu sang dang so
+        float so_a = Float.parseFloat(strsoA);
+        float so_b = Float.parseFloat(strsoB);
+        //Tinh toan theo yeu cau
+        float Tong  = so_a * so_b;
+        String strTong = String.valueOf(Tong);
+        //Hien thi ra man hinh
+        editTextKQ.setText(strTong);
     }
-    void XulyChia(View view){
+    public void XulyChia(View view){
 
+        //Lay du lieu ve o dieu khien ve a
+        String strsoA = editTextsoA.getText().toString();
+        //Lay du lieu ve o dieu khien ve b
+        String strsoB =editTextsoB.getText().toString();
+        //Chuyen du lieu sang dang so
+        float so_a = Float.parseFloat(strsoA);
+        float so_b = Float.parseFloat(strsoB);
+        //Tinh toan theo yeu cau
+        float Tong  = so_a / so_b;
+        String strTong = String.valueOf(Tong);
+        //Hien thi ra man hinh
+        editTextKQ.setText(strTong);
     }
 }
