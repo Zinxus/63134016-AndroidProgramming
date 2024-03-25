b39ffb431a31d9823720151c9fe5b88d0fa8f3d8
Zinxus <hieu123lio71@gmail.com>
2024-03-25 21:30:52 +0700

    them  cac nut de lang nghe su kien

--- AndroidStudioProjects/Cau2_AppCalculator/app/src/main/java/ntu_63134016/myapplication/MainActivity.java
+++ AndroidStudioProjects/Cau2_AppCalculator/app/src/main/java/ntu_63134016/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu_63134016.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultTV, solutionTV;
+    MaterialButton buttonC, buttonBrackOpen, buttonBrackClose;
+    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
+    MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonAC,buttonDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }
