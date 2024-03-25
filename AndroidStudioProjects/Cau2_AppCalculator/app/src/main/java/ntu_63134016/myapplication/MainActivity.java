55556843bc2ec8be56cea6673797e5d9ffc6981b
Zinxus <hieu123lio71@gmail.com>
2024-03-25 22:16:53 +0700

    Hoan thanh xu ly ket qua khi in ra

--- AndroidStudioProjects/Cau2_AppCalculator/app/src/main/java/ntu_63134016/myapplication/MainActivity.java
+++ AndroidStudioProjects/Cau2_AppCalculator/app/src/main/java/ntu_63134016/myapplication/MainActivity.java
@@ -92,6 +92,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             context.setOptimizationLevel(-1);
             Scriptable scriptable = context.initStandardObjects();
             String finalResult = context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult =finalResult.replace(".0","");
+            }
             return finalResult;
         }catch (Exception e){
             return "Error";
