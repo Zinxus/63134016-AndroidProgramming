6db3f7852d05f11ae309fef898c29c0610dbb1fb
Zinxus <hieu123lio71@gmail.com>
2024-03-25 21:45:48 +0700

    hoan thanh lang nghe lay du lieu  truyen du lieu

--- AndroidStudioProjects/Cau2_AppCalculator/app/src/main/java/ntu_63134016/myapplication/MainActivity.java
+++ AndroidStudioProjects/Cau2_AppCalculator/app/src/main/java/ntu_63134016/myapplication/MainActivity.java
@@ -25,15 +25,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        resultTV = findViewById(R.id.result_tv);
+        solutionTV = findViewById(R.id.solution_tv);
+
+        assignIn(buttonC,R.id.buton_c);
+        assignIn(buttonBrackOpen,R.id.buton_open_bracket);
+        assignIn(buttonBrackClose,R.id.buton_close_bracket);
+        assignIn(buttonDivide,R.id.buton_divide);
+        assignIn(buttonMultiply,R.id.buton_multiply);
+        assignIn(buttonPlus,R.id.buton_plus);
+        assignIn(buttonMinus,R.id.buton_minus);
+        assignIn(buttonEquals,R.id.buton_equal);
+        assignIn(button0,R.id.buton_0);
+        assignIn(button1,R.id.buton_1);
+        assignIn(button2,R.id.buton_2);
+        assignIn(button3,R.id.buton_3);
+        assignIn(button4,R.id.buton_4);
+        assignIn(button5,R.id.buton_5);
+        assignIn(button6,R.id.buton_6);
+        assignIn(button7,R.id.buton_7);
+        assignIn(button8,R.id.buton_8);
+        assignIn(button9,R.id.buton_9);
+        assignIn(buttonAC,R.id.buton_ac);
+        assignIn(buttonDot,R.id.buton_dot);
+
+
+
+    }
+
+    void assignIn(MaterialButton btn, int id){
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        
+        MaterialButton button =(MaterialButton) v;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTV.getText().toString();
+
+        if(buttonText.equals("AC")){
+            solutionTV.setText("");
+        }
+
+        dataToCalculate = dataToCalculate+buttonText;
+        solutionTV.setText(dataToCalculate);
     }
 }
