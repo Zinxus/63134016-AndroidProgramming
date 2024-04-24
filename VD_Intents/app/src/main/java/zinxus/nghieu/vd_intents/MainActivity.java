afdc2a74eb7cff4d0c70a9436aabb2a2ee6ae302
Zinxus <hieu123lio71@gmail.com>
2024-04-24 20:52:36 +0700

    Sữa lỗi hoàn thiện

--- VD_Intents/app/src/main/java/zinxus/nghieu/vd_intents/MainActivity.java
+++ VD_Intents/app/src/main/java/zinxus/nghieu/vd_intents/MainActivity.java
@@ -3,23 +3,25 @@ package zinxus.nghieu.vd_intents;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnChinh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-    }
-    public void ChuyenManHinhso1(View v){
-        Intent iManHinhKhac = new Intent(this, SubActivity1.class);
-        startActivity(iManHinhKhac);
+        btnChinh = findViewById(R.id.btnchinh);
+        btnChinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iManHinhKhac = new Intent(MainActivity.this, SubActivity1.class);
+                startActivity(iManHinhKhac);
+            }
+        });
     }
 }
--- VD_Intents/app/src/main/java/zinxus/nghieu/vd_intents/SubActivity1.java
+++ VD_Intents/app/src/main/java/zinxus/nghieu/vd_intents/SubActivity1.java
@@ -3,23 +3,25 @@ package zinxus.nghieu.vd_intents;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SubActivity1 extends AppCompatActivity {
-
+    Button btnSub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sub1);
-    }
-    public void ChuyenManHinhso2(View v){
-        Intent iManHinhChinh = new Intent(this, MainActivity.class);
-        startActivity(iManHinhChinh);
+        btnSub = findViewById(R.id.btnsub);
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iManHinhChinh = new Intent(SubActivity1.this, MainActivity.class);
+                startActivity(iManHinhChinh);
+            }
+        });
     }
 }
