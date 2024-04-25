09a69c119848a1073391932f7bee9fd3043ceeae
Zinxus <hieu123lio71@gmail.com>
2024-04-25 20:53:55 +0700

    Hoàn Thành VD2

--- VD2_Intents/app/src/main/java/zinxus/nghieu/vd2_intents/MainActivity.java
+++ VD2_Intents/app/src/main/java/zinxus/nghieu/vd2_intents/MainActivity.java
@@ -3,6 +3,7 @@ package zinxus.nghieu.vd2_intents;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,22 +14,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnchuyen_nl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-    }
-    public void Nhaplieu(View v){
-        Intent iNhap = new Intent( this, NhaplieuActivity.class);
-        startActivity(iNhap);
+        btnchuyen_nl = findViewById(R.id.btnchuyen_nl);
+        btnchuyen_nl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iNhap = new Intent( MainActivity.this, NhaplieuActivity.class);
+                startActivityForResult(iNhap, 8000);
+            }
+        });
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode==8000)
-            if (resultCode==RESULT_OK){
+        super.onActivityResult(requestCode, resultCode,data);
+        if(requestCode==8000 && resultCode==RESULT_OK){
                 String hovatenNhanduoc = data.getStringExtra("HT");
                 int namsinhnhanduoc = data.getIntExtra("NS",2020);
 
@@ -38,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
                 tvNS.setText(String.valueOf(namsinhnhanduoc));
             }
         else
-                Toast.makeText(this,"Trả về thất bại",Toast.LENGTH_LONG);
-        else
-            super.onActivityResult(requestCode, resultCode,data);
+                Toast.makeText(this,"Trả về thất bại",Toast.LENGTH_LONG).show();
     }
+
+
 }
--- VD2_Intents/app/src/main/java/zinxus/nghieu/vd2_intents/NhaplieuActivity.java
+++ VD2_Intents/app/src/main/java/zinxus/nghieu/vd2_intents/NhaplieuActivity.java
@@ -3,6 +3,7 @@ package zinxus.nghieu.vd2_intents;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -12,26 +13,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class NhaplieuActivity extends AppCompatActivity {
+    EditText edHT, edNS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_nhaplieu);
+        Button btnchuyen_chinh = findViewById(R.id.btnchuyen_chinh);
+        btnchuyen_chinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy dữ liệu từ EditTexts
+                edHT = findViewById(R.id.edTHovaTen);
+                edNS = findViewById(R.id.edTNamSinh);
+                String hoten = edHT.getText().toString();
+                int namsinh = Integer.parseInt(edNS.getText().toString());
+
+                // Tạo Intent để trả lại dữ liệu
+                Intent iKQNhaplieu = new Intent();
+                iKQNhaplieu.putExtra("HT", hoten);
+                iKQNhaplieu.putExtra("NS", namsinh);
+
+                // Đặt kết quả là RESULT_OK và gửi dữ liệu về
+                setResult(RESULT_OK, iKQNhaplieu);
+
+                // Kết thúc Activity và quay trở lại MainActivity
+                finish();
+            }
+        });
     }
-    public void ManHinhChinh(View v){
-        Intent iManHinhChinh = new Intent( this, MainActivity.class);
-        startActivity(iManHinhChinh);
-    }
-    public void Nhap_Quayve(View v){
-        EditText edHT = (EditText) findViewById(R.id.edTHovaTen);
-        EditText edNS = (EditText) findViewById(R.id.edTNamSinh);
-        String hoten = edHT.getText().toString();
-        int namsinh = Integer.parseInt(edNS.getText().toString());
-        Intent iKQNhaplieu = new Intent();
-        iKQNhaplieu.putExtra("HT", hoten);
-        iKQNhaplieu.putExtra("NS", namsinh);
-        setResult(RESULT_OK,iKQNhaplieu);
-        finish();
-    }
+
 }
